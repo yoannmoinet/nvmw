92fc26b2b00ce81e8c1eb3a6fc0968f567b7db04
Yoann Moinet <yoann.moinet@gmail.com>
2015-11-28 20:34:03 -0500

    support node v4+

--- nvmw.bat
+++ nvmw.bat
@@ -132,9 +132,17 @@ if %NODE_TYPE% == iojs (
   )
 ) else (
   if %ARCH% == x32 (
-    set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/node.exe
+    if not "%NODE_VERSION:~1,1%" == "0" (
+      set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/win-x86/node.exe
+    ) else (
+      set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/node.exe
+    )
   ) else (
-    set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/x64/node.exe
+    if not "%NODE_VERSION:~1,1%" == "0" (
+      set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/win-x64/node.exe
+    ) else (
+      set NODE_EXE_URL=%NVMW_NODEJS_ORG_MIRROR%/%NODE_VERSION%/x64/node.exe
+    )
   )
 )
 
